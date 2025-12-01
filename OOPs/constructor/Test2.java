75a3b857b99cad710c03cd6b4e62b429987bdb01
Soumyaranjan Behera <beherasoumyaranjan650@gmail.com>
2025-12-01 18:35:21 +0530

    this and super keyword

---
+++ OOPs/constructor/Test2.java
@@ -0,0 +1,58 @@
+/**this and super keyword
+ * ->The this keyword is used to come the current class variable 
+ * -> super keyword is used to call the parent class variable
+ */
+
+// class Test2{
+//     int x = 10;
+//     public void m1(){
+//         int x = 20;
+//         System.out.println(x);
+//         System.out.println(this.x);
+//     }
+//     public static void main(String args[]){
+//         Test2 t = new Test2();
+//         t.m1();
+//     }
+// }
+
+
+
+
+// class A{
+//     int x = 10;
+// }
+// class Test2 extends A{
+//     int x = 20;
+//     public void m1(){
+//         int x = 30;
+//         System.out.println(x);
+//         System.out.println(this.x);
+//         System.out.println(super.x);
+//     }
+//     public static void main(String args[]){
+//         Test2 t = new Test2();
+//         t.m1();
+//     }
+// }
+
+
+
+
+//this and super keyword can use anywhere inside the class except static area.
+// class A{
+//     int x = 10;
+// }
+// class Test2 extends A{
+//     int x = 20;
+//     public static void m1(){
+//         System.out.println(this.x);
+//         System.out.println(super.x);
+//     }
+//     public static void main(String args[]){
+//         Test2 t =new Test2();
+//         t.m1();
+//     }
+// }
+
+
