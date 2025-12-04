acb762cc1f9f51ba42bc5a3cd9d20d282ea958b6
Soumyaranjan Behera <beherasoumyaranjan650@gmail.com>
2025-12-04 22:14:03 +0530

    object type casting

---
+++ OOPs/constructor/Obj-typecasting/Demo.java
@@ -0,0 +1,41 @@
+/**Object type casting 
+ *   The conversion of one type of object into another type is the concept of object type casting.
+ * ->to type caste any object mainly we have to follow 3 rules ,where first two rules
+ *   will check by compiler and last rule will check by jvm.
+ */
+
+// class Demo{
+//     public static void main(String args[]){
+//         Object o = new String();
+//         String s = (String)o;
+//     }
+// }
+
+
+
+// class Demo{
+//     public static void main(String args[]){
+//         Object o = new String();
+//         String s = o;
+//     }
+// }
+
+
+
+// class Demo{
+//     public static void main(String args[]){
+//         Object o = new Demo();
+//         String s = (String)o;
+//     }
+// }
+
+
+
+// class Demo{
+//     public static void main(String args[]){
+//         Object o = new Demo();
+//         String s = (Object)o;
+//     }
+// }
+
+
