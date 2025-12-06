426736be1ad519754ccb9d2f31867968b86a9959
Soumyaranjan Behera <beherasoumyaranjan650@gmail.com>
2025-12-06 20:44:43 +0530

    Abstraction and data hiding

---
+++ OOPs/Abstraction/Demo.java
@@ -0,0 +1,7 @@
+/**Abstraction
+ * ->Hiding the internal implementation and showing only functionality 
+ *  to the user is the concept of Abstraction.
+ * ->We can achive abstraction by the help of abstract class and interface.
+ * ->by using abstract class we can achive 0 to 100 percentage abstraction but
+ *   by the help of interface we can achive fully 100 percent abstraction.
+ */
---
+++ OOPs/Data Hiding/Demo.java
@@ -0,0 +1,10 @@
+/**Data hiding 
+ * -> it is the concept of hiding our data in such a way , that no outside 
+ *      class can able to access our data.
+ * ->We can achive data hiding by declaring our variable as private
+ * 
+ */
+
+class Demo{
+    private int roll = 10;//data hiding
+}
