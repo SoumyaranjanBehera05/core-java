4c74812b93541bbd2260b15ad956f74e2949558a
Soumyaranjan Behera <beherasoumyaranjan650@gmail.com>
2025-12-06 20:53:23 +0530

    Polymorphism

---
+++ OOPs/Polymorphism/Demo.java
@@ -0,0 +1,9 @@
+/**it is the concept of achiving method resolution in different ways 
+ * 
+ * poly means many
+ * morphism means forms
+ * 
+ * ->These are two types
+ *      1-Compile time polymorphism
+ *      2-Run time polymorphism
+*/
